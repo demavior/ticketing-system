baad1971a700a0f5817287d2f97108275e4c62f6
Martin <vivasdema@gmail.com>
2024-12-30 14:44:42 -0500

    Add user authentication endpoints and registration serializer

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from .serializers import UserRegistrationSerializer
 
-# Create your views here.
+class RegistrationView(APIView):
+    def post(self, request):
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class LoginView(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        response = super(LoginView, self).post(request, *args, **kwargs)
+        token = Token.objects.get(key=response.data['token'])
+        user = token.user
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'email': user.email,
+            'role': user.role,
+            'tenants': [tenant.name for tenant in user.tenants.all()]
+        })
+    
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        try:
+            # Get the token from the request
+            token = Token.objects.get(user=request.user)
+            # Delete the token to log out the user
+            token.delete()
+            return Response(status=status.HTTP_200_OK)
+        except Token.DoesNotExist:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
