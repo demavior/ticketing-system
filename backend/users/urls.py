e2b396d0dfa3beb828948b8b16549ded7f4d5b71
Martin <vivasdema@gmail.com>
2024-12-31 10:05:06 -0500

    Add user detail and list views with serializers, and generate Endpoints. Enhance tenant handling in middleware.

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import RegistrationView, LoginView, LogoutView
+from .views import RegistrationView, LoginView, LogoutView, UserDetailView, UserListView
 
 urlpatterns = [
     path('register/', RegistrationView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('detail/', UserDetailView.as_view(), name='user-detail'),
+    path('detail/<int:user_id>/', UserDetailView.as_view(), name='user-detail'),
+    path('list/', UserListView.as_view(), name='user-list'),
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -4,10 +4,12 @@ from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
-from .serializers import UserRegistrationSerializer
+from .models import CustomUser
+from .serializers import UserRegistrationSerializer, UserDetailSerializer, TenantUserSerializer
 
 class RegistrationView(APIView):
     def post(self, request):
+        request.data['tenants'] = [request.tenant.id]
         serializer = UserRegistrationSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -32,10 +34,49 @@ class LogoutView(APIView):
 
     def post(self, request):
         try:
-            # Get the token from the request
+            # Get the token from the request, and delete it
             token = Token.objects.get(user=request.user)
-            # Delete the token to log out the user
             token.delete()
             return Response(status=status.HTTP_200_OK)
         except Token.DoesNotExist:
             return Response(status=status.HTTP_400_BAD_REQUEST)
+
+class UserDetailView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, user_id=None):
+        if user_id:
+            try:
+                user = CustomUser.objects.get(id=user_id)
+            except CustomUser.DoesNotExist:
+                return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
+        else:
+            user = request.user
+        serializer = UserDetailSerializer(user)
+        return Response(serializer.data)
+        
+    def put(self, request, user_id=None):
+        if user_id:
+            try:
+                user = CustomUser.objects.get(id=user_id)
+            except CustomUser.DoesNotExist:
+                return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
+        else:
+            user = request.user
+        serializer = UserDetailSerializer(user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UserListView(APIView):
+    def get(self, request):
+        # Get all users by tenant
+        if request.tenant:
+            users = CustomUser.objects.filter(tenants=request.tenant)
+            serializer = TenantUserSerializer(users, many=True)
+            return Response(serializer.data)
+        # Get all users
+        users = CustomUser.objects.all()
+        serializer = TenantUserSerializer(users, many=True)
+        return Response(serializer.data)
